cc3a949da518874725647627524b12e77beae711
Joy Allen <joy.allen@ncl.ac.uk>
2017-05-13 13:53:15 +0100

    ss update
    update sample size

--- ShinyApps/SampleSizeScript/server.R
+++ ShinyApps/SampleSizeScript/server.R
@@ -7,7 +7,23 @@
 #    http://shiny.rstudio.com/
 #
 
-
+samplesize <- function(prev, SnI, CI, alpha, beta){
+  
+  z_a = qnorm(1-alpha/2, mean = 0, sd = 1, log = FALSE) # currently set up for two tail
+  z_b = qnorm(1-beta, mean = 0, sd = 1, log = FALSE)
+  
+  
+  n_obs = (z_a*sqrt(SnI*(1-SnI)) + z_b*sqrt((SnI-CI)*(1-(SnI-CI))))^2/CI^2
+  n = n_obs/prev  
+  
+  s_size = 
+    data.frame(
+      Name = c("Number of events required", 
+               "Sample size required"),
+      Value = c(n_obs, 
+                n))
+  return(s_size)
+}
 library(shiny)
 # Define server logic for slider examples
 
