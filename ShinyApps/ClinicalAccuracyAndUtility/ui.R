bb17c06a38f270318a61beda353b3c41f4d1b1d7
HMichaelPower <Michael.Power@ncl.ac.uk>
2017-07-01 22:35:39 +0100

    Restructured the navigation bars and menus

--- ShinyApps/ClinicalAccuracyAndUtility/ui.R
+++ ShinyApps/ClinicalAccuracyAndUtility/ui.R
@@ -1,13 +1,10 @@
 #################### ui ###############################
 
 ui <- function(request) {
-  fluidPage(
-#  tags$head(tags$style(HTML(mycss))),
-  titlePanel(h4("Evaluating a diagnostic test: clinical accuracy and clinical utility")),
-
-  sidebarLayout(
-    sidebarPanel(
-      actionButton("GoButton", "Update the graph"),
+  navbarPage(h4("Evaluating a diagnostic test"),
+    tabPanel("About", includeHTML("www/tab1.html")), 
+    
+    tabPanel("Input data",
       tags$br(),
       tags$h3("Input Variables"),
       textInput(inputId = "DxCondition", label = "Condition", value = "Example disease"),
@@ -23,25 +20,34 @@ ui <- function(request) {
       numericInput("RuleOutDecisionThreshold", "Rule-out NPV threshold", min=0, max=1, value= 0.1, step = 0.01),
       bookmarkButton()
     ),
-    mainPanel(
-      tabsetPanel(
-        tabPanel("About", includeHTML("www/tab1.html")), 
-        tabPanel("About graph 1", includeHTML("www/tab2.html")), 
+    
+    navbarMenu("Clinical accuracy",
+        tabPanel("About graph 1", includeHTML("www/tab2.html")),
         tabPanel("Graph 1: clinical accuracy",
-                 withSpinner(plotOutput("PrePostProb"))),
-        tabPanel("About graph 2", includeHTML("www/tab3.html")), 
+                 actionButton("GoButton", "Update the graph"),
+                 withSpinner(plotOutput("PrePostProb"))
+                 # bookmarkButton()    # does not work here
+        )),
+    
+    navbarMenu("Clinical utility",
+        tabPanel("About graph 2", includeHTML("www/tab3.html")),
         tabPanel("Graph 2: clinical utility", #div(id = "plot-container",
-                 withSpinner(plotOutput("RuleInOutPlot")))
-        # tabPanel("Tables", dataTableOutput("linesTable"))
-      ), 
+                 actionButton("GoButton", "Update the graph"),
+                 withSpinner(plotOutput("RuleInOutPlot"))
+                 # bookmarkButton()    # does not work here
+    )),
+    
+
+###################################
+#
+#     credits as a running footer
+#
       tags$br(),
       tags$b("Cite as:"),
       tags$p("Michael Power, Joy Allen."),
       tags$em("A ShinyApp tool to explore dependence of rule-in and rule-out decisions on prevalence, sensitivity, specificity, and confidence intervals"),
       tags$p("NIHR Diagnostic Evidence Co-operative Newcastle. July 2017"),
-      #tgs$br(),
-      tags$img(src = "nihr-logo.jpg", width = "80px", height = "28px", align = "right") # add the NIHR logo
-    )
-  )
-)
-}
+      tags$br(),
+      tags$img(src = "nihr-logo.jpg", width = "80px", height = "28px", align = "right") # add the NIHR logo)
+    
+)}
