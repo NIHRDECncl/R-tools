8471d76ac26ab30a347da9be6572eefa8801303b
HMichaelPower <Michael.Power@ncl.ac.uk>
2017-07-02 07:06:56 +0100

    Restructured Inputs tab
    Recalculate and bookmarks bar relocated to Inputs tab

--- ShinyApps/ClinicalAccuracyAndUtility/ui.R
+++ ShinyApps/ClinicalAccuracyAndUtility/ui.R
@@ -4,37 +4,59 @@ ui <- function(request) {
   navbarPage(h4("Explore the clinically useful measures of test accuracy"),
     tabPanel("About", includeHTML("www/tab1.html")), 
     
-    tabPanel("Input data: set sensitivity, specificity, etc",
-      tags$br(),
-      tags$h3("Input Variables"),
-      textInput(inputId = "DxCondition", label = "Condition", value = "Example disease"),
-      textInput(inputId = "DxTestName", label = "Name of test", value = "Example test"),
-      numericInput("prevalence", "prior probability (prevalence)", min=0, max=1, value=0.4, step = 0.01),
-      numericInput("n", "population", min=1, max=1000, value=300),
-      numericInput("sensitivity", "sensitivity of index test", min=0, max=1, value= 0.90, step = 0.01),
-      numericInput("specificity", "specificity of index test", min=0, max=1, value= 0.80, step = 0.01),
-      textInput(inputId = "DxRuleInDecision", label = "Rule-in decision", value = "Example: start treatment"),
-      numericInput("RuleInDecisionThreshold", "Rule-in PPV threshold", min=0, max=1, value= 0.5, step = 0.01),
-      textInput(inputId = "IndeterminateDecision", label = "Indeterminate decision", value = "Example: investigate further"),
-      textInput(inputId = "DxRuleOutDecision", label = "Rule-out decision", value = "Example: rule out the condition"),
-      numericInput("RuleOutDecisionThreshold", "Rule-out NPV threshold", min=0, max=1, value= 0.1, step = 0.01),
-      bookmarkButton()
-    ),
+    tabPanel("Input labels, sensitivity, specificity, etc",
+             fluidRow(
+               column(4,actionButton("GoButton", "Update the graphs")),
+               column(4, bookmarkButton())
+             ),
+             
+             fluidRow(
+               hr(),
+               column(4, wellPanel(
+                                   textInput(inputId = "DxCondition", label = "Name of condition", value = "Example: Disease"),
+                                   textInput(inputId = "DxTestName", label = "Name of test", value = "Example: Test")
+               )),
+               column(4, wellPanel(
+                                   numericInput("prevalence", "Prior probability (prevalence)", min=0, max=1, value=0.4, step = 0.01),
+                                   numericInput("n", "Study size", min=1, max=1000, value=300)
+               )),
+               
+               column(4, wellPanel(
+                                   numericInput("sensitivity", "Sensitivity of test", min=0, max=1, value= 0.90, step = 0.01),
+                                   numericInput("specificity", "Specificity of test", min=0, max=1, value= 0.80, step = 0.01)
+               ))
+             ),
+               
+               fluidRow( 
+                 h5(" Data for clinical decisions"),
+                 column(4, wellPanel(
+                                     textInput(inputId = "DxRuleInDecision", label = "Rule-in decision", value = "Example: start treatment"),
+                                     numericInput("RuleInDecisionThreshold", "Rule-in PPV threshold", min=0, max=1, value= 0.5, step = 0.01)
+                 )),
+                 
+                 column(4, wellPanel(
+                                     textInput(inputId = "IndeterminateDecision", label = "Indeterminate decision", value = "Example: investigate further")
+                 )),
+                 
+                 column(4, wellPanel(
+                                     textInput(inputId = "DxRuleOutDecision", label = "Rule-out decision", value = "Example: rule out the condition"),
+                                     numericInput("RuleOutDecisionThreshold", "Rule-out NPV threshold", min=0, max=1, value= 0.1, step = 0.01)
+                 ))
+                 
+                )
+      ),
     
     navbarMenu("Clinical accuracy: predictive values",
         tabPanel("About graph 1", includeHTML("www/tab2.html")),
         tabPanel("Graph 1: clinical accuracy: pre- and post-test probabilities; predictive values",
-                 actionButton("GoButton", "Update the graph"),
                  withSpinner(plotOutput("PrePostProb"))
                  # bookmarkButton()    # does not work here
         )),
     
-    navbarMenu("Clinical utility: thresholds for clinical decisons",
+    navbarMenu("Clinical utility: thresholds to cross for clinical decisons",
         tabPanel("About graph 2", includeHTML("www/tab3.html")),
         tabPanel("Graph 2: clinical utility: post-test probabilities and decision thresholds", 
-                 actionButton("GoButton", "Update the graph"),
                  withSpinner(plotOutput("RuleInOutPlot"))
-                 # bookmarkButton()    # does not work here
     )),
     
 
