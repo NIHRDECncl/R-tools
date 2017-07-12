64b4b5eba5fe54ea6b12fddb6b04673e0dd5cc08
HMichaelPower <Michael.Power@ncl.ac.uk>
2017-07-12 15:21:01 +0100

    Put inputs & outputs into their own tab
    Conditional survival

--- ShinyApps/Conditional survival/server.R
+++ ShinyApps/Conditional survival/server.R
@@ -4,10 +4,11 @@ server <- function(input, output, session) {
   session$onSessionEnded(stopApp) # it can be annoying that when you close the browser window, the app is still running and you need to manually press “Esc” to kill it
   observeEvent("", {
     wd <- getwd()
-    path <- paste0(getwd(), "/www/ConditionalSurvival.xlsx")
+    path <- paste0(getwd(), "/data/ConditionalSurvival.xlsx")
     sheets <- excel_sheets(path)
     conditions <- read_excel(path, sheets[1])
     survivalData <- read_excel(path, sheets[2])
+    viewpoints <- read_excel(path, sheets[3])
     output$conditions <- renderTable(conditions)
     output$survivalData <- renderTable(survivalData)
   }, ignoreNULL = FALSE)
