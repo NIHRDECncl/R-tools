1e4804b192827a808e827535ada2e1b0f504110c
HMichaelPower <Michael.Power@ncl.ac.uk>
2017-07-12 15:54:32 +0100

    Tidy UI design

--- ShinyApps/Conditional survival/ui.R
+++ ShinyApps/Conditional survival/ui.R
@@ -2,38 +2,30 @@
 
 ui <- function(request) { 
   navbarPage("",
+    navbarMenu("About",
+        tabPanel("Introduction",
+          h6("under construction: Introduction")
+                        ),
+         tabPanel("Interpreting prognosis statistics",
+           h6("under construction: Interpreting prognosis statistics"))),       
       tabPanel("Explore prognosis",
       sidebarLayout(
        sidebarPanel(
          wellPanel(
-           h6("under construction"),
-           actionButton("goPrint", "Save file for printing"),
+           h6("under construction: inputs"),
+           actionButton("goPrint", "Print"),
            bookmarkButton()
          ),
-         
-         
         h5("under construction: choices for picklists will be read from a spreadsheet; when the choice is made, the input data will be dynakically populated"),
-        selectInput("condition", label = "Condition", choices = c("generic", "ovarian cancer: high grade, stage IIIc")),
-        selectInput("outcome", label = "Outcome", choices = c("Survival", "Disease-free survical")),
-        selectInput("baseline for outcome", label = "Baseline for survival", choices = c("new diagnosis", "responded to treatment", "response to treatment poor", "relapse")),
         wellPanel(
-          h6("Enter management strategies"),
-          textInput("RxStrategy1", "", "Treatment strategy 1"),
-          textInput("RxStrategy2", "", "Treatment strategy 2")
-        ),
-        wellPanel(
-          selectInput("units", label = "Units for survival statistics", choices = c("years", "months", "days")),
-          h5("under construction: ui will be constructed in server to dynamically update the labels for the following:"),
-          h6("Survival estimates are a range of numbers, not a single number that would imply unwarrented certainty"),
-          sliderInput("surviveHorizon", "Survival horizon", min = 0, max = 10, value = 5, step = 1),
-          sliderInput("surviveNoRx", "% survival with no treatment", min = 0, max = 100, value = c(1, 5), step = 1),
-          sliderInput("surviveRx1", "% survival with strategy 1", min = 0, max = 100, value = c(10, 20), step = 1),
-          sliderInput("surviveRx2", "% survival with strategy 2", min = 0, max = 100, value = c(15, 25), step = 1)
+          selectInput("condition", label = "Condition", choices = c("generic", "ovarian cancer: high grade, stage IIIc")),
+          selectInput("outcome", label = "Outcome", choices = c("Survival", "Disease-free survical")),
+          selectInput("group", label = "Subgroup", choices = c("age", "stage", "histology"))
         )
       ),
       mainPanel(
         wellPanel(
-                  h6("Plots under construction: "),
+                  h6("under construction: plots"),
                               
                   tags$br("lines A1, A2, A3  will show survival curves over the horizon for the different treatment strategies"),
                   tags$br("lines B2, B3  will show survival expectation curves over the horizon for the different treatment strategies"),
@@ -44,32 +36,36 @@ ui <- function(request) {
       ),
   navbarMenu("Viewpoints", 
             tabPanel("view1 title contributor",
-                h6("under construction")),
+                h6("under construction: contributor list 1")),
             tabPanel("view1 title contributor",
-                h6("under construction"))),
+                h6("under construction: contributor list 2"))),
  navbarMenu("Contacts",
             tabPanel("Maintainers",
-                 h6("under construction")
+                 h6("under construction: maintainers")
             ),
             tabPanel("Developers",
-                h6("under construction"))),
-  navbarMenu("More",
-            tabPanel("Sources of data",
-            tableOutput("conditions"),
-            tableOutput("survivalData"),
-            h6("For the condition chosen: the sources, links to sources, contributor contact details")
-            ),
+                h6("under construction: developers"))),
+ navbarMenu("Contribute",
             tabPanel("Contribute addotional data on survival",
-               h5("under construction"),
+               h5("under construction: contribute data"),
                 h6("Contributors will submit data on a spreadsheet with"),
                h6("* everything in the data input section"),
               h6("* a bibliograpy with references to sources and links to them"),
                h6("* their contact details which will be published")
                ),
              tabPanel("Linking, licencing, and fair use",
-                h6("under construction"))),
+                h6("under construction: licensing"))),
  
-    
+ navbarMenu("Outputs for debugging",
+            tabPanel("Sheets",
+                     tableOutput("sheets")),
+            tabPanel("Conditions",
+                     tableOutput("conditions")),
+            tabPanel("Survival data",
+                     tableOutput("survivalData")),
+            tabPanel("Viewpoints",
+                     tableOutput("Viewpoints"))),
+ 
     
     ###################################
     #
