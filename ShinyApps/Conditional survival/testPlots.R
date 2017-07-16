56ed933fab5a6a99145fd7df409f50298c4b3b26
HMichaelPower <Michael.Power@ncl.ac.uk>
2017-07-16 16:31:47 +0100

    Facet wrap (when necessary)

--- ShinyApps/Conditional survival/testPlots.R
+++ ShinyApps/Conditional survival/testPlots.R
@@ -1,9 +1,26 @@
 ################# server for ShinyApp to explore survival aand conditional survival
 
+library(shiny)
+library(tidyverse) # Imports: broom, DBI, dplyr, forcats, ggplot2, haven, httr, hms, jsonlite, lubridate, magrittr, modelr, purrr, readr, readxl, stringr, tibble, rvest, tidyr, xml2
+library(rsconnect)   # needed to upload to Shinyio
+library(readr)       # needed for GET()
+library(vcd)         # mosaic() plot http://www.statmethods.net/advgraphs/mosaic.html
+library(colourpicker) # http://deanattali.com/blog/plot-colour-helper/ 
+library(shinythemes)
+library(DT)
+library(shinycssloaders)
+#      library(proportion)  package no longer being maintained :-(
+library(PropCIs)
+library(rsconnect)   # needed to upload to Shinyio
+library(readxl)
+
+
+
 # read spreadsheet
 
     wd <- getwd()
     path <- paste0(getwd(), "/data/ConditionalSurvival.xlsx")
+    path <- paste0("/Users/michaelpower/Google Drive/GIT-project/GitHub/R-tools/ShinyApps/Conditional survival", "/data/ConditionalSurvival.xlsx")
     sheets <- data.frame(excel_sheets(path), stringsAsFactors = FALSE)
     names(sheets) <- "sheets"
     
@@ -42,6 +59,10 @@
     
 pPlot <- ggplot(pData, aes(time, proportion, group = legend4Line, colour = legend4Line, fill = legend4Line)) 
 pPlot <- pPlot + labs(x = pXlab, y = pYlab, colour = pLegendTitle, fill = NULL)
+
+if (sum(is.na(pData$group2)) == 0) 
+  pPlot <- pPlot + facet_wrap(~group1, ncol = 1)
+
 pPlot <- pPlot + geom_line() + geom_point()
 pPlot <- pPlot + geom_ribbon(
   aes(ymin = wbMin, ymax = wbMax, fill = factor(legend4Line), colour = factor(legend4Line)), 
@@ -65,6 +86,10 @@ csLegendTitle <- csMetadata$title4Legend[1] ##########  check
 csPlot <- ggplot(csData, aes(time, proportion, group = legend4Line, colour = legend4Line, fill = legend4Line)) 
 # csPlot <- csPlot + labs(x = csXlab, y = csYlab, colour = "Stage and\nage group", fill = NULL)
 csPlot <- csPlot + labs(x = csXlab, y = csYlab, colour = csLegendTitle, fill = NULL)
+
+if (sum(is.na(csData$group2)) == 0) 
+  csPlot <- csPlot + facet_wrap(~group1, ncol = 2)
+
 csPlot <- csPlot + geom_line() + geom_point()
 
 csPlot <- csPlot + geom_ribbon(
