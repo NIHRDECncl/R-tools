d8ee9c4e1aa07f932cce652812889d9bf43c67b3
HMichaelPower <Michael.Power@ncl.ac.uk>
2017-07-16 12:33:58 +0100

    Pretty prognosis and conditional survival plots
    Need to find out how to get newline from excel intoR

--- ShinyApps/Conditional survival/testPlots.R
+++ ShinyApps/Conditional survival/testPlots.R
@@ -7,60 +7,48 @@
     sheets <- data.frame(excel_sheets(path), stringsAsFactors = FALSE)
     names(sheets) <- "sheets"
     
-    plotsMetadata <- read_excel(path, sheets$sheets[1])
-    plotsData <- read_excel(path, sheets$sheets[2])
+    metadata4Plots <- read_excel(path, sheets$sheets[1])
+    data4Plots <- read_excel(path, sheets$sheets[2])
 
     #  select data to plot
     
-    conditionChoices <- plotsMetadata$Condition %>% 
-          sort %>% 
-            unique
-    
-    conditionChoices
+    conditionChoices <- metadata4Plots$condition %>% 
+          sort() %>% 
+            unique()
     conditionChoice <- conditionChoices[1]
     
-    prognosisPlots <- subset(plotsMetadata, Condition == conditionChoice & View == "Prognosis")$Plot_Dataset %>% 
-          sort
-    prognosisPlots
-    prognosisPlotChoice <- prognosisPlots[1]
+    prognosisPlotChoices <- 
+      subset(metadata4Plots, condition == conditionChoice & view == "Prognosis")$plotNameAndDataset %>% 
+          sort()
+    prognosisPlotChoice <- prognosisPlotChoices[1]
     
-    datasetChoice <- subset(plotsMetadata, Plot_Dataset == prognosisPlotChoice)$Dataset
+    datasetChoice <- subset(metadata4Plots, plotNameAndDataset == prognosisPlotChoice)$dataset
     datasetChoice
+ 
+    pData <- subset(data4Plots, plotNameAndDataset == prognosisPlotChoice)
+    pMetaData <- subset(metadata4Plots, plotNameAndDataset == prognosisPlotChoice)
+      
+      csPlotChoices <- 
+      subset(metadata4Plots, condition == conditionChoice & view == "Conditional survival")$plotNameAndDataset %>% 
+      sort()
+      csPlotChoice <- csPlotChoices[1]
+  
+    csData <- subset(data4Plots, plotNameAndDataset == csPlotChoice)
+    csMetaData <- subset(metadata4Plots, plotNameAndDataset == csPlotChoice)
     
-    pData <- subset(plotsData, PlotName == prognosisPlotChoice)
-
+    pXlab <- pMetaData$xLabel[1]
+    pYlab <- pMetaData$yLabel[1]
+    pGroup1Name <- pData$legend4Line[1] ##########  check
     
-    pXlab <- subset(plotsMetadata, Plot_Dataset == prognosisPlotChoice)$Xlab
-    pYlab <- subset(plotsMetadata, Plot_Dataset == prognosisPlotChoice)$Ylab
-    pGroup1Name <- subset(plotsMetadata, Plot_Dataset == prognosisPlotChoice)$Group1Name
-    pGroup2Name <- subset(plotsMetadata, Plot_Dataset == prognosisPlotChoice)$Group2Name
-    pPlotTitle <- subset(plotsMetadata, Plot_Dataset == prognosisPlotChoice)$PlotTitle
-    
-    
-conditionalSurvivalPlots <- subset(plotsMetadata, Condition == conditionChoice & View == "Conditional survival" & Dataset == datasetChoice)$Plot_Dataset %>% 
-    sort 
-
-conditionalSurvivalPlots
-conditionalSurvivalPlotChoice <- conditionalSurvivalPlots[1]
-
-csData <- subset(plotsData, PlotName == conditionalSurvivalPlotChoice)
-
-csXlab <- subset(plotsMetadata, Plot_Dataset == conditionalSurvivalPlotChoice)$Xlab
-csYlab <- subset(plotsMetadata, Plot_Dataset == conditionalSurvivalPlotChoice)$Ylab
-csGroup1Name <- subset(plotsMetadata, Plot_Dataset == conditionalSurvivalPlotChoice)$Group1Name
-csGroup2Name <- subset(plotsMetadata, Plot_Dataset == conditionalSurvivalPlotChoice)$Group2Name
-csPlotTitle <- subset(plotsMetadata, Plot_Dataset == conditionalSurvivalPlotChoice)$PlotTitle
-
-
-pPlot <- ggplot(pData, aes(Time, Proportion, group = Group1, colour = Group1, fill = Group1)) 
+pPlot <- ggplot(pData, aes(time, proportion, group = legend4Line, colour = legend4Line, fill = legend4Line)) 
 pPlot <- pPlot + labs(x = pXlab, y = pYlab, colour = pGroup1Name, fill = NULL)
 pPlot <- pPlot + geom_line() + geom_point()
 pPlot <- pPlot + geom_ribbon(
-  aes(ymin = WBmin, ymax = WBmax, fill = factor(Group1), colour = factor(Group1)), 
+  aes(ymin = wbMin, ymax = wbMax, fill = factor(legend4Line), colour = factor(legend4Line)), 
   alpha = 1/10, linetype = 0)
 
 pPlot <- pPlot + geom_ribbon(
-  aes(ymin = CILL, ymax = CIUL, fill = factor(Group1), colour = factor(Group1)), 
+  aes(ymin = ciMin, ymax = ciMax, fill = factor(legend4Line), colour = factor(legend4Line)), 
   alpha = 1/10, linetype = 0)
 
 pPlot <- pPlot + scale_fill_discrete(breaks = NULL)
@@ -68,5 +56,26 @@ pPlot <- pPlot + scale_fill_discrete(breaks = NULL)
 pPlot
 
 
-                  
+
+
+csXlab <- csMetaData$xLabel[1]
+csYlab <- csMetaData$yLabel[1]
+csGroup1Name <- csData$legend4Line[1] ##########  check
+
+csPlot <- ggplot(csData, aes(time, proportion, group = legend4Line, colour = legend4Line, fill = legend4Line)) 
+csPlot <- csPlot + labs(x = csXlab, y = csYlab, colour = csGroup1Name, fill = NULL)
+csPlot <- csPlot + geom_line() + geom_point()
+
+csPlot <- csPlot + geom_ribbon(
+  aes(ymin = wbMin, ymax = wbMax, fill = factor(legend4Line), colour = factor(legend4Line)), 
+  alpha = 1/10, linetype = 0)
+
+csPlot <- csPlot + geom_ribbon(
+  aes(ymin = ciMin, ymax = ciMax, fill = factor(legend4Line), colour = factor(legend4Line)), 
+  alpha = 1/10, linetype = 0)
+
+csPlot <- csPlot + scale_fill_discrete(breaks = NULL)
+
+csPlot
+
   
