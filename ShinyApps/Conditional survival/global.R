3615fc89e0060cf5b0a6b472069efeb25b4922d9
HMichaelPower <Michael.Power@ncl.ac.uk>
2017-07-18 09:14:16 +0100

    options(shiny.reactlog = TRUE)

--- ShinyApps/Conditional survival/global.R
+++ ShinyApps/Conditional survival/global.R
@@ -5,28 +5,6 @@ if( !exists("LoadPackages", mode="function")) source("FunctionsUsedByConditional
 LoadPackages()
 enableBookmarking("url")
 options(shiny.error = browser)
-
-# load data as global unreactive objects
-
-path <- paste0(getwd(), "/data/ConditionalSurvival.xlsx")
-# path <- paste0("/Users/michaelpower/Google Drive/GIT-project/GitHub/R-tools/ShinyApps/Conditional survival", "/data/ConditionalSurvival.xlsx")
-sheets <- data.frame(excel_sheets(path), stringsAsFactors = FALSE)
-names(sheets) <- "sheets"
-
-metadata4Plots <- read_excel(path, sheets$sheets[1])
-data4Plots <- read_excel(path, sheets$sheets[2])
-
-datasetChoices <- subset(metadata4Plots, plotNameAndDataset == prognosisPlotChoice)$dataset %>% 
-  sort() %>% 
-  unique()
-                                        
-conditionChoices <- metadata4Plots$condition %>% 
-  sort() %>% 
-  unique()
-
-prognosisPlotChoices <- 
-  subset(metadata4Plots, condition == conditionChoice & view == "Prognosis")$plotNameAndDataset %>% 
-  sort()
-
+options(shiny.reactlog = TRUE)
 
                     
